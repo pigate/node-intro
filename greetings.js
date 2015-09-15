2981739ef35de8b61731d5cfe0c4f102605d2519
pigate <mimchiang@gmail.com>
2015-09-14 23:33:38 -0700

    refactor greetings.js to use nested

--- greetings.js
+++ greetings.js
@@ -1,10 +1,11 @@
 //exports is the same thing as module.exports
 //var exports = module.exports = {}
 
-exports.sayHelloInEnglish = function(){
-  return "Hello";
-};
-
-exports.sayHelloInSpanish = function(){
-  return "Hola";
+module.exports = {
+  sayHelloInEnglish: function(){
+    return "Hello";
+  },
+  sayHelloInSpanish: function(){
+    return "Hola";
+  }
 };
